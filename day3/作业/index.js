b077366b3ebeebe545b77192acdab6bd2f602924
JiangWen <13089848409@163.com>
2020-11-05 10:06:25 +0800

    day2-work

---
+++ day3/作业/index.js
@@ -0,0 +1,31 @@
+// var arr = [ 1, 4, 5, 2 ];
+
+// arr[i]   arr[0]
+// loop1: (i=0) {
+//     loop1 ( i = 0; j = 0  ): 1 < 4 ? [ 4, 1, 5, 2 ]     (0, 1)
+//     loop2（ i = 0; j = 1  ）: 1 < 5 ? [ 4, 5, 1, 2 ]     (1, 2)
+//     loop3(i = 0; j = 2 ): 1 < 2 ? [ 4, 5, 2, 1 ](2, 3)
+//                                                 { j, j+1}
+// }
+
+// loop2: {
+//     loop1: 4 < 5 ? [ 5, 4, 2, 1 ]
+//     loop2: 4<2? [5, 4, 2, 1]
+// }
+
+// loop3: {
+//     loop1: 5<4? [5, 4, 2, 1]
+// }
+var arr = [ 1, 4, 19, 10, 7, 3, 5, 2 ];
+    
+for (var i = 0; i < arr.length - 1; i++) {
+    for (var j = 0; j < arr.length - i - 1; j++){
+        if (arr[ j ] > arr[ j + 1 ]) {
+            var temp = arr[ j ];
+            arr[ j ] = arr[ j + 1 ];
+            arr[ j + 1 ] = temp;
+        }
+    }
+}
+console.log('最终得到的排序结果：', arr);
+
---
+++ day3/作业/work.js
@@ -0,0 +1,98 @@
+// 1
+// function isPrime(num) {
+//     for (var j = 2; j < num; j++) {
+//         if (num % j == 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// console.log('311-' + isPrime(311))
+// console.log('3-' + isPrime(3))
+// console.log('9-' + isPrime(9))
+
+
+// 2 排序
+// var array = [ 1, 3, 5, 4, 8, 7, 2 ];
+// for (let i = 0; i < array.length - 1; i++) {
+
+//     for (let j = 0; j < array.length - 1 - i; j++) {
+
+//         if (array[j] > array[j + 1]) {
+//             let temp = array[j];
+//             array[j] = array[j + 1];
+//             array[j + 1] = temp;
+//         }
+//     }
+// }
+// console.log("排序后的数组", array);
+
+
+
+
+// 3 计算天和小时
+// var time = 109;
+// var day = Math.floor(109 / 24)
+// var hour = 109 % 24;
+
+// console.log(`共有${day}天${hour}小时`)
+
+
+
+
+// 4 99乘法表
+// for (var i = 1; i <= 9; i++) {
+//     for (var j = 1; j <= i; j++) {
+//         console.log(`${i}*${j}=${i*j}`)
+//     }
+// }
+
+
+
+
+// 5 是否是偶数
+// function isEvenNum(num) {
+//     if (num % 2 == 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+
+
+// 附加题
+function fun2() {
+    for (var i = 1; i < 100; i++) {
+        var count = 0;
+        for (var j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                count++;
+            }
+            if (j == i && count == 2) {
+                console.log(i)
+            }
+        }
+    }
+}
+
+function fun3() {
+    for (var i = 2; i < 100; i++) {
+        var a = true;
+        for (var j = 2; j < i; j++) {
+            //判断i能否被j整除
+            if (i % j == 0) {
+                //能被整除则说明不是素数，修改布尔值为false
+                a = false;
+            }
+        }
+        //打印素数
+        if (a) {
+            console.log(i);
+        }
+    }
+}
+
+// fun2();
+// console.log('------------------------------------------')
+// fun3();
